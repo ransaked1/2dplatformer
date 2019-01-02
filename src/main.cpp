e50bc3e8eca4f64b82dfbcd42bb7dccb5fb852f5
root <root@LAPTOP-3LN59P5O.localdomain>
2019-01-01 23:15:39 -0500

    update 1

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 /*TO DO!
 -put everything physics related in applyPhysics
--learn level building or generate procedualy
--prepare everything for pluging in AI
--create a ground class?
+-learn level building or generate randomly
+-prepare input for pluging in AI
+-create a ground class and objects?
 */
 
 void	applyPhysics(sf::RectangleShape *player, sf::RectangleShape *ground, double *velocityY)
@@ -17,20 +17,21 @@ void	applyPhysics(sf::RectangleShape *player, sf::RectangleShape *ground, double
 
 int main()
 {
-	/************************\
-	|*** INITIALIZATIONS ****|
-	\************************/
+	/***********************\
+	**** INITIALIZATIONS ****
+	\***********************/
 
 	sf::ContextSettings settings;
 	settings.antialiasingLevel = 8;
-	sf::RenderWindow window(sf::VideoMode(720, 720), "SFML 2D Platformer!");	//initializing window
-	sf::View view(sf::FloatRect(50.f, 100.f, 500.f, 500.f));
+	sf::RenderWindow window(sf::VideoMode(720, 720), "SFML 2D Platformer!");	//initializing window size and name
+	sf::View view(sf::FloatRect(50.f, 100.f, 500.f, 500.f));                  //initializing the field of view
 
 	sf::RectangleShape player;			//creating the controllable shape
 	player.setSize(sf::Vector2f(50, 25));
 	player.setPosition(50, 300);
+	player.setFillColor(sf::Color::Red);
 
-	sf::RectangleShape ground;			//the lomng ground object
+	sf::RectangleShape ground;			//the long ground object
 	ground.setSize(sf::Vector2f(1000, 30));
 	ground.setPosition(0, 400);
 
@@ -46,32 +47,20 @@ int main()
 	double velocityY = 0;					//player velocity in Y direction
 
 	sf::Font font;
-	font.loadFromFile("fonts/arial.ttf");		//scoring text
+	font.loadFromFile("fonts/arial.ttf");		//scoring text setup
 	sf::Text text;
 	text.setFont(font);
 	text.setCharacterSize(25);
 
 	int score = 0;
 
-
-	while (window.isOpen())		//infinite loop while window is opens
+	//infinite loop while window is open
+	while (window.isOpen())
 	{
-		sf::Event event;
+		sf::Event event;	//event is a buffer checking key states
 
-		//If the object is accelerating upward and is not on the ground then it's in the air
-		if (velocityY < 0 && onGround == false)
-			inAir = true;
-		else
-			inAir = false;
-
-		//Checking if player is intersection a ground object and testing if its over the ground
-		if ((player.getGlobalBounds().intersects(ground.getGlobalBounds()) && ground.getPosition().y - 20 >= player.getPosition().y) ||
-		(player.getGlobalBounds().intersects(ground2.getGlobalBounds()) && ground2.getPosition().y - 20 >= player.getPosition().y))
-				onGround = true;
-		else
-			onGround = false;
-
-		while (window.pollEvent(event)) //checking if there is any event before updating the frame
+		//checking if there is any button pressed before updating the frame
+		while (window.pollEvent(event))
 		{
 			switch (event.type)
 			{
@@ -90,14 +79,15 @@ int main()
 			}
 
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))    // left and right movement
-				velocityX = 1.3;
+				velocityX = 2;
 			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-				velocityX = -1.3;
+				velocityX = -2;
 			else
 				velocityX = 0;
 		}
 
-		if (onGround == false ||  inAir == true)   //if object is not on the ground or in the air apply gravity
+		//applying physics
+		if (onGround == false || inAir == true)   //if object is not on the ground or in the air apply gravity
 		{
 			player.move(velocityX, velocityY);
 
@@ -108,30 +98,44 @@ int main()
 		else
 			player.move(velocityX, 0);		//if no change in Y move on X axis
 
-		if (player.getPosition().y > 440) //reset player after dying
+		//reset player after falling off (bellow pixel 440 from top)
+		if (player.getPosition().y > 440)
 			player.setPosition(50, 300);
 
+		//If the object is accelerating upward and is not on the ground then it's in the air
+		if (velocityY < 0 && onGround == false)
+			inAir = true;
+		else
+			inAir = false;
+
+		//Checking if player is intersection a ground object and testing if its over the ground
+		if ((player.getGlobalBounds().intersects(ground.getGlobalBounds()) && ground.getPosition().y - 22 >= player.getPosition().y) ||
+		(player.getGlobalBounds().intersects(ground2.getGlobalBounds()) && ground2.getPosition().y - 22 >= player.getPosition().y))
+			onGround = true;
+		else
+			onGround = false;
+
+		//debuging
 		//cout << player.getPosition().x << " " << player.getPosition().y << endl;
 		//cout << ground2.getPosition().x << " " << ground2.getPosition().y << endl;
+		//cout << "onGround: " << onGround << "   " << "inAir: " << inAir << endl;
 
-		cout << "onGround: " << onGround << "   " << "inAir: " << inAir << endl; //debuging
-
-		view.setCenter(player.getPosition().x + 80, player.getPosition().y - 80); //set screen following player
+		//post processing
+		view.setCenter(player.getPosition().x + 80, player.getPosition().y - 80); 		//set screen following player
 
 		score = (player.getPosition().x - 50) / 100;  //scoring formula
 		text.setString("Score: " + to_string(score));
-		text.setPosition(player.getPosition().x - 120, player.getPosition().y - 300); // print score and follow the player
+		text.setPosition(player.getPosition().x - 120, player.getPosition().y - 300);	// print score and follow the player
 
 		window.setView(view); //set scene
 		window.clear();				//clear scene
 
-		window.draw(player);		//set objects to be drawn
+		window.draw(player);	//set objects to be drawn
 		window.draw(ground);
 		window.draw(ground2);
 		window.draw(text);
 
 		window.display();			//draw the scene
 	}
-
 	return 0;
 }
